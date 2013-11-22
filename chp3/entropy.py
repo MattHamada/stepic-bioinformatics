b19357d43d47b91b1d8190c49bb4cf88af4d48a8
MattHamada <ermacaz@gmail.com>
2013-11-22 14:00:50 -0700

    entropy of matrix completed

--- chp3/entropy.py
+++ chp3/entropy.py
@@ -1,3 +1,4 @@
+import math
 motifs = (('T','C','G','G','G','G','G','T','T','T','T','T'),
                  ('C','C','G','G','T','G','A','C','T','T','A','C'),
                  ('A','C','G','G','G','G','A','T','T','T','T','C'),
@@ -9,12 +10,32 @@ motifs = (('T','C','G','G','G','G','G','T','T','T','T','T'),
                  ('T','A','G','G','G','G','A','A','C','T','A','C'),
                  ('T','C','G','G','G','T','A','T','A','A','C','C'))
 
-#A = 0 C = 1 G = 2 T = 3
-countMatrix = [[], [], [], []]
-for i in range(len(motifs)):
-    for j in range(len(motifs[i])):
-        if motifs[i][j] == 'A'
+
+countMatrix = [{'A': 0, 'T': 0, 'C': 0, 'G': 0},{'A': 0, 'T': 0, 'C': 0, 'G': 0},{'A': 0, 'T': 0, 'C': 0, 'G': 0},
+                          {'A': 0, 'T': 0, 'C': 0, 'G': 0},{'A': 0, 'T': 0, 'C': 0, 'G': 0},{'A': 0, 'T': 0, 'C': 0, 'G': 0},
+                          {'A': 0, 'T': 0, 'C': 0, 'G': 0},{'A': 0, 'T': 0, 'C': 0, 'G': 0},{'A': 0, 'T': 0, 'C': 0, 'G': 0},
+                          {'A': 0, 'T': 0, 'C': 0, 'G': 0},{'A': 0, 'T': 0, 'C': 0, 'G': 0},{'A': 0, 'T': 0, 'C': 0, 'G': 0}]
+for i in range(len(motifs[0])):
+    for j in range(len(motifs)):
+        countMatrix[i][motifs[j][i]] += 1 
     
+profileMatrix =  [{'A': 0, 'T': 0, 'C': 0, 'G': 0},{'A': 0, 'T': 0, 'C': 0, 'G': 0},{'A': 0, 'T': 0, 'C': 0, 'G': 0},
+                          {'A': 0, 'T': 0, 'C': 0, 'G': 0},{'A': 0, 'T': 0, 'C': 0, 'G': 0},{'A': 0, 'T': 0, 'C': 0, 'G': 0},
+                          {'A': 0, 'T': 0, 'C': 0, 'G': 0},{'A': 0, 'T': 0, 'C': 0, 'G': 0},{'A': 0, 'T': 0, 'C': 0, 'G': 0},
+                          {'A': 0, 'T': 0, 'C': 0, 'G': 0},{'A': 0, 'T': 0, 'C': 0, 'G': 0},{'A': 0, 'T': 0, 'C': 0, 'G': 0}]
 
+for i in range(len(profileMatrix)):
+    for hash in countMatrix:
+        for key in hash:
+            profileMatrix[i][key] = countMatrix[i][key] / 10.0
 
+entropies = []
+for hash in profileMatrix:
+    entropy = 0
+    for key in hash:
+        if not hash[key]  == 0:
+            entropy += hash[key] * math.log(hash[key],2)
+    entropies.append(entropy)
+
+print sum(entropies)
                 
