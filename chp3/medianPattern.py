71bc6cc8faff007316cdf62c73deccfbf2946a8b
MattHamada <ermacaz@gmail.com>
2013-11-22 17:11:16 -0700

    median pattern done

--- chp3/medianPattern.py
+++ chp3/medianPattern.py
@@ -1,35 +1,47 @@
 import itertools
+
+
+def seqScore(dna, pattern):
+    bestScore = 10000
+    for i in range(len(dna)-len(pattern)+1):
+        kmer = dna[i:i+len(pattern)]
+        kmerScore = 0
+        for j in range(len(kmer)):
+            if kmer[j] != pattern[j]:
+                kmerScore += 1
+        if kmerScore < bestScore:
+            bestScore = kmerScore
+    return bestScore
+
+
 def medianPattern(dnas, k):
     bestScore = 10000
     bestPattern = ''
-    patterns = itertools.permutations('ATGC', k)
+    patterns = itertools.product('ATGC', repeat=k)
     for pattern in patterns:
+        #print 'pattern:', pattern
         score = 0
         for dna in dnas:
-            for i in range(len(dna)-k):
-                kmer = dna[i:i+k]
-                for i in range(len(pattern)):
-                    #print 'i:', i
-                    #print 'k:', k
-                    #print 'kmer:', kmer
-                    #print 'pattern:', pattern
-                    if kmer[i] != pattern[i]:
-                        score += 1
-        if pattern == ('G','A','C'):
-            print score
+          score += seqScore(dna, pattern)
         if score < bestScore:
             bestScore = score
             bestPattern = pattern
-            print pattern
-    return pattern
-
-dnas = ['AAATTGACGCAT',
-              'GACGACCACGTT',
-              'CGTCAGCGCCTG',
-              'GCTGAGCACCGG',
-              'AGTACGGGACAG']
-a = medianPattern(dnas, 3)
+        #print bestScore
+    return bestPattern
+dnas = ['TTACCTTCTTGGGTCGCCCAGATTATAATCCGTGAGAGAAAC',
+'TACAATCTTTTGCGCAGCTTACCTTTGTTTAGGCATCGACCC',
+'TTAACTGCTAGTCCGCATAAGATTCCTTTAAGACTATGCCTA',
+'TCTCTGCGCCGTGCCGACTTACCTGCGGCAGGCGCGTTAAAC',
+'TTACCTTACTCTGGACCAGCATATAGCAAGCGAAAACGCGAC',
+'TTATCTGTGTCGCGGGGACTGAAGCCTCCGTTCACCTAAATC',
+'ATTAATTTAACTCCTATGAAATGAACCATCTCTTTCTAAGGT',
+'GAAGGAATTAATAGTGGGAAGTCCCTCTTGTAAGACTTACCT',
+'TGGGCACCTGGATAATGGCCAGACTTAGCTCCGTATCTAGAA',
+'TCCTTGTTACAGACTGGGAAGCCAGGATAATTATCTGTAAAG']
+##dnas = ['ttaccttaac',
+##'gatatctgtc',
+##'acggcgttcg',
+##'ccctaaagag',
+##'cgtcagaggt',]
+a = medianPattern(dnas, 6)
 print a
-    
-                
-    
